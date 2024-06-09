874c416ec36a8230742ba780d60e2136ffba2586
Mohamed Fawzy <47891524+Mohamadfawzy@users.noreply.github.com>
2024-06-09 20:19:12 +0300

    create project

---
+++ source/PharmaStoreInventory/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PharmaStoreInventory
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ source/PharmaStoreInventory/Views/StockDetails.xaml.cs
@@ -0,0 +1,20 @@
+namespace PharmaStoreInventory.Views;
+
+public partial class StockDetails : ContentPage
+{
+	public StockDetails()
+	{
+		InitializeComponent();
+	}
+    private void OnFavoriteSwipeItemInvoked(object sender, EventArgs e)
+    {
+        DisplayAlert("title", "massage", "cancel");
+    }
+
+    private void OnDeleteSwipeItemInvoked(object sender, EventArgs e)
+    {
+        DisplayAlert("title", "massage", "cancel");
+
+    }
+
+}
